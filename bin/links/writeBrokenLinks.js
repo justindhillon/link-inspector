f89da44904d2abbcb1fd7c0443efd1d73e7c68ea
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-11-01 23:19:07 -0700

    write to output folder

--- bin/links/writeBrokenLinks.js
+++ bin/links/writeBrokenLinks.js
@@ -1,17 +1,33 @@
 const linkCheck = require('link-check');
+const fs = require('fs');
+const path = require('path');
 
-// link is string
+// data is string
 // path is <file/directory path>
-// Writes link to 
-function writeToFile(link, path) {
-    console.log(link, path);
+// Writes data to identical path in "output"
+function writeToFile(data, PATH) {
+    PATH = "output/" + PATH;
+    const directoryPath = path.dirname(PATH);
+
+    // Create the directory if it doesn't exist
+    if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true });
+    }
+
+    // Writes to file
+    fs.appendFile(PATH, data + "\n", function (err) {
+        if (err) {
+            console.error('Error: failed to write to', path, err);
+            process.exit(1);
+        }
+    });
 }
 
 // links is array of strings
 // path is <file/directory path>
 // If broken links are found, it writes them
 // in an "output" folder
-async function writeBrokenLinks(links, path) {
+async function writeBrokenLinks(links, PATH) {
     for (const link of links) {
         linkCheck(link, function (err, result) {
             if (err) {
