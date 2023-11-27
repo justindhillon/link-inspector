6387f402bc4dd76bbbf4ac1e95a772ecbf6621fc
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-11-26 16:08:10 -0800

    make test better

--- test/scanLinks.test.js
+++ test/scanLinks.test.js
@@ -1,4 +1,5 @@
 const scanLinks = require("../bin/scan/scanLinks");
+const fs = require('fs');
 
 // Documentation on dircompare:
 // https://gliviu.github.io/dc-api/index.html
@@ -7,6 +8,15 @@ const options = { compareContent: true };
 
 describe('scanLinks.js', () => {
     test('scanLinks returns broken links', async () => {
+        // Remove "output/test_suite"
+        fs.rm("output/test_suite", { recursive: true, force: true }, (err) => {
+            if (err) {
+              // An error occurred
+              console.error(err);
+              process.exit(1);
+            }
+          });
+
         await scanLinks("test/test_suite").then(async () => {
             const res = dircompare.compareSync("test/expected_output", "output/test_suite", options);
             console.log(res.same);
