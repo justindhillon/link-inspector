de1a3ef058eea2d66fe990868c721274d359952a
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-10 20:56:32 -0800

    runs recursivly on directories

--- linkInspector.ts
+++ linkInspector.ts
@@ -1,5 +1,25 @@
 import { checkLink } from "./checkLink";
+import fs from 'fs';
 
 export function linkInspector(arg: string) {
-    checkLink(arg);
+    try { // If arg is a link
+        new URL(arg);
+        checkLink(arg);
+    } catch {}
+
+    try { // If arg is a path
+        const stats = fs.statSync(arg);
+
+        if (stats.isDirectory()) {
+            const files: string[] = fs.readdirSync(arg);
+            for (const file of files) {
+                linkInspector(arg + "/" + file);
+            }
+        }
+
+        
+    } catch (err) {
+        console.error(err);
+        console.error("Error: Not a valid link or path")
+    }
 }
