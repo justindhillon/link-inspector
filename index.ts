24471e489678f868b062e1143816e66a26e9caee
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-15 17:43:37 -0800

    refactor linkInspector

--- index.ts
+++ index.ts
@@ -1,59 +1,34 @@
 import { checkLink } from "./checkLink";
 import fs from 'fs';
 
-const MAX_CONCURRENT_REQUESTS = 10;
+export async function linkInspector(arg: string) {
+    try { // If arg is a link
+        new URL(arg);
+        await checkLink(arg);
+        return;
+    } catch {}
 
-export async function linkInspector(arg: string): Promise<string[]> {
-    const linkQueue: string[] = [arg]; // Initialize the queue with the initial argument
+    try { // If arg is a path
+        const stats = fs.statSync(arg);
 
-    const brokenLinks: string[] = [];
-
-    async function processQueue() {
-        const currentArg = linkQueue.shift();
-
-        if (!currentArg) {
-            return; // Queue is empty
-        }
-
-        try {
-            const urlObject = new URL(currentArg);
-            if (await checkLink(currentArg)) {
-                brokenLinks.push(currentArg);
+        // Handle directory
+        if (stats.isDirectory()) {
+            const files: string[] = fs.readdirSync(arg);
+            for (const file of files) {
+                await linkInspector(arg + "/" + file);
             }
-        } catch {
-            try {
-                const stats = fs.statSync(currentArg);
-
-                if (stats.isDirectory()) {
-                    const files: string[] = fs.readdirSync(currentArg);
-                    linkQueue.push(...files.map(file => `${currentArg}/${file}`));
-                } else {
-                    const content: string = fs.readFileSync(currentArg, 'utf8');
-                    const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-                    const links: string[] = content.match(urlRegex) || [];
-                    linkQueue.push(...links);
-                }
-            } catch (err) {
-                console.error(currentArg);
-                console.error(err);
-                console.error("Error: Not a valid link or path");
-            }
-        }
-    }
-
-    async function executeWithConcurrency() {
-        const promises: Promise<void>[] = [];
-
-        for (let i = 0; i < MAX_CONCURRENT_REQUESTS; i++) {
-            promises.push(processQueue());
+            return;
         }
 
-        await Promise.all(promises);
-    }
+        // Handle file
+        const content: string = fs.readFileSync(arg, 'utf8');
+        const urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        const links: string[] = content.match(urlRegex) || [];
 
-    while (linkQueue.length > 0) {
-        await executeWithConcurrency();
+        for (const link of links) {
+            await linkInspector(link);
+        }
+    } catch (err) {
+        console.error("Error: Not a valid link or path")
     }
-
-    return brokenLinks;
 }
