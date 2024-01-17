e092b07326d4c789fdbbed88dee28240979bf29a
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-17 12:09:04 -0800

    add tests

--- test/test.ts
+++ test/test.ts
@@ -1,15 +1,48 @@
 import { linkInspector } from "../index";
 
-let brokenLinks: string[] = [];
+const expectedLinks = [
+    'https://www.bbc.com/nonexistent-article-abcxyz',
+    'https://www.bbc.com/nonexistent-article-abcxyz',
+    'https://www.nasa.gov/unknown-page',
+    'https://www.nasa.gov/unknown-page',
+    'https://www.nationalgeographic.com/404-error-page-not-found',
+    'https://www.nationalgeographic.com/404-error-page-not-found',
+    'https://www.un.org/en/thispagedoesnotexist',
+    'https://www.un.org/en/thispagedoesnotexist',
+    'https://www.wikipedia.org/404testpage12345',
+    'https://www.wikipedia.org/404testpage12345'
+]
 
-function addLink(link: string) {
-    brokenLinks.push(link);
+const expectedPaths = [
+    'test/tests/brokenLinks',
+    'test/tests/subfolder/brokenLinks',
+    'test/tests/brokenLinks',
+    'test/tests/subfolder/brokenLinks',
+    'test/tests/brokenLinks',
+    'test/tests/subfolder/brokenLinks',
+    'test/tests/brokenLinks',
+    'test/tests/subfolder/brokenLinks',
+    'test/tests/brokenLinks',
+    'test/tests/subfolder/brokenLinks'
+]
+
+function test() {
+    brokenLinks.sort();
+
+    for (const i in brokenLinks) {
+        const link = brokenLinks[i]![0];
+        const path = brokenLinks[i]![1];
+        if (link != expectedLinks[i]) throw Error;
+        if (path != expectedPaths[i]) throw Error;
+    }
 }
 
-// Assuming linkInspector returns a Promise
-async function inspectLinks() {
-    await linkInspector("https://www.nationalgeographic.com/404-error-page-not-found", addLink);
-    console.log(brokenLinks);
+let brokenLinks: string[][] = [];
+
+function addLinks(link: string, path: string) {
+    brokenLinks.push([link, path]);
 }
 
-inspectLinks();
+linkInspector("test/tests", addLinks);
+
+setTimeout(test, 10000);
