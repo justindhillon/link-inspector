5979dca42d11c5a998c8d4fdfc51103ef32a5a8d
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-01-23 13:55:26 -0800

    undo bad fix

--- index.ts
+++ index.ts
@@ -66,7 +66,7 @@ export default async function linkInspector(arg: string, callback: any, path='')
                 new URL(link);
                 PROCESSES.push([link, pathAfterDirectory]);
                 runProcess(callback);
-            } catch { return }
+            } catch {}
         }
     } catch {
         console.error("Error: Not a valid link or path")
